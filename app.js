2462fa90a1a2a0295c364be49234226d451e21be
Abheekrishna <abheekrishna17@gmail.com>
2023-06-10 12:39:06 +0530

    FEATURE: added the rainbow mode to the canvas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('btn');
 const drawBtn = document.getElementById('draw');
 const canvasDiv = document.getElementById('canvas');
+const rainbowBtn = document.getElementById('rainbow');
 
 
 canvasDiv.style.display = 'grid';
@@ -9,14 +10,33 @@ canvasDiv.style.gridTemplateRows = 'repeat(30, 1fr)';
 canvasDiv.style.boxShadow = '0 10px 25px rgba(0, 0, 0, 0.3)';
 
 
+function rainbow () {
+    const rgb = [];
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    rgb.push(r);
+    rgb.push(g);
+    rgb.push(b);
+    const rainbowColor = rgb.join(' ');
+    return rainbowColor;
+}
+
 drawBtn.addEventListener('click', () =>{
 for (i = 0; i < 30; i++) {
     for (j = 0; j < 30; j++) {
         const div = document.createElement('div');
-        div.classList.add('grid-cell');
         div.addEventListener('mousemove', () => {
             div.style.backgroundColor = 'rgb(57, 57, 57)';
         })
+
+        rainbowBtn.addEventListener('click', () => {
+            div.addEventListener('mousemove', () => {
+                div.style.backgroundColor = `rgb(${rainbow()})`
+            })
+        })
+
+        //Clear
         btn.addEventListener('click', () => {
             div.style.backgroundColor = 'transparent';
         })
@@ -24,3 +44,4 @@ for (i = 0; i < 30; i++) {
     }
 }
 })
+

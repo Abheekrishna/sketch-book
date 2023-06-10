22e95373f5de30c99a1bb5ca6a32d39a55b993a2
Abheekrishna <abheekrishna17@gmail.com>
2023-06-10 12:05:44 +0530

    ADDED: btn for start drawing on the canvas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('btn');
+const drawBtn = document.getElementById('draw');
 const canvasDiv = document.getElementById('canvas');
 
 
@@ -7,16 +8,19 @@ canvasDiv.style.gridTemplateColumns = 'repeat(30, 1fr)';
 canvasDiv.style.gridTemplateRows = 'repeat(30, 1fr)';
 canvasDiv.style.boxShadow = '0 10px 25px rgba(0, 0, 0, 0.3)';
 
+
+drawBtn.addEventListener('click', () =>{
 for (i = 0; i < 30; i++) {
     for (j = 0; j < 30; j++) {
         const div = document.createElement('div');
         div.classList.add('grid-cell');
         div.addEventListener('mousemove', () => {
-            div.classList.add('grid-active')
+            div.style.backgroundColor = 'rgb(57, 57, 57)';
         })
         btn.addEventListener('click', () => {
-            div.style.backgroundColor = '#ffffff'
+            div.style.backgroundColor = 'transparent';
         })
         canvasDiv.appendChild(div);
     }
 }
+})
